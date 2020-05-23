d79f2c81b2bb65b6bc972732cb33f4a149952812
Matheus Frauches <frauches.27@gmail.com>
2020-05-23 18:58:16 -0300

    Create base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import 'react-native-gesture-handler';
+import Routes from './routes/index';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{flex: 1}}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
