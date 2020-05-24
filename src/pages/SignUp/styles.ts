efe8e69954c77d540d532ea079962c280522c69e
Matheus Frauches <frauches.27@gmail.com>
2020-05-24 16:37:18 -0300

    Add styles to SignUp page

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,39 @@
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const BackToSignInText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #f4ede8;
+  margin-left: 16px;
+`;
