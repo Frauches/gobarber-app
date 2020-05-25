6099896459c1865b095ca3bcf1cb0cf11edfd028
Matheus Frauches <frauches.27@gmail.com>
2020-05-25 16:45:52 -0300

    Create AuthContext and apply its provider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import { StatusBar, View } from 'react-native';
 import 'react-native-gesture-handler';
 import Routes from './routes/index';
+import AppProvider from './context/index';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{flex: 1}}>
-        <Routes />
-      </View>
+      <AppProvider>
+        <View style={{ flex: 1 }}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   );
 };
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,84 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import React, {
+  useEffect,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
+import api from './../services/api';
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function loadStoragedData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@GoBarber:token',
+        '@GoBarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+    }
+
+    loadStoragedData();
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const { data } = await api.post<{ token: string; user: object }>(
+      '/sessions',
+      { email, password },
+    );
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:token', data.token],
+      ['@GoBarber:user', JSON.stringify(data.user)],
+    ]);
+
+    setData({ token: data.token, user: data.user });
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@GoBarber:user', '@GoBarber:token']);
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { useAuth, AuthProvider };
